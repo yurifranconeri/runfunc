83133b511e6eb22a852ac59279579d3df3761eb7
Yuri Franconeri <yurifranconeri@google.com>
2025-03-07 02:40:05 -0300

    v0.2

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ def hello_get(request):
         Functions, see the `Writing HTTP functions` page.
         <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
     """
-    return "Hello World!"
+    return "Hello World! Deploy GitHub"
 
